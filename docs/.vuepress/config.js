0c4efb26296f6539ac360a92e98c98f2c0597f5b
jinwanyang <jinwanyang@tencent.com>
2020-09-21 17:55:19 +0800

    docs: change config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   base: '/docs/',
   title: 'mrapi',
   description: 'mrapi docs',
-  head: [['link', { rel: 'icon', href: '/favico.ico' }]],
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
   locales: {
     '/': {
       lang: 'en-US',
